930a160adcb7add3aaab66f79793b75bee098783
kazumakabashima <k.kazuma1028@gmail.com>
2023-07-05 15:48:39 +0900

    ユーザー管理機能修正01
    ## What
    レビューに基づき修正しました。
    コードレビューをお願い致します。
    
    ## Why
    ### 正規表現
    - first_name_kanji, last_name_kanji :  validationを修正
    - password :  validationを修正
    
    ### Faker
    - passwordに必ず数字と英語が入力されるように修正
    
    ### Spec
    - context を実装
    
    ### Password テスト
    - 単体テストを修正
    
    ### Gayzo
    - テスト実行時のターミナルhttps://gyazo.com/532456f47c200c072a62438979e2f616

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :nickname,         presence: true
-  validates :password,                         format: { with: /\A(?=.*[a-zA-Z])(?=.*\d)/ } 
-  validates :first_name_kanji, presence: true, format: { with: /\A[ぁ-んァ-ン一-龯]+\z/ }
-  validates :last_name_kanji,  presence: true, format: { with: /\A[ぁ-んァ-ン一-龯]+\z/ }
+  validates :password,                         format: { with: /\A(?=.*[a-z])(?=.*\d)/ }
+  validates :first_name_kanji, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+  validates :last_name_kanji,  presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
   validates :first_name_kana,  presence: true, format: { with: /\A[ァ-ヶ]+\z/ }
   validates :last_name_kana,   presence: true, format: { with: /\A[ァ-ヶ]+\z/ }
   validates :birth_day,        presence: true
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     nickname              { Faker::Name.initials(number: 2)}
     email                 { Faker::Internet.email}
-    password              { Faker::Internet.password(min_length: 6) }
+    password              { '1a' + Faker::Internet.password(min_length: 6) }
     password_confirmation { password }
     first_name_kanji      { "単体" }
     last_name_kanji       { "試験" }

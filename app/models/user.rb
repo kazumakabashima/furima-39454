ee17f84b521b86daffefea1404ba129759e21787
kazumakabashima <k.kazuma1028@gmail.com>
2023-07-05 16:40:07 +0900

    正規表現、難しい。

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
   validates :password,                         format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
   validates :first_name_kanji, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
   validates :last_name_kanji,  presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
-  validates :first_name_kana,  presence: true, format: { with: /\A[ァ-ヶ]+\z/ }
-  validates :last_name_kana,   presence: true, format: { with: /\A[ァ-ヶ]+\z/ }
+  validates :first_name_kana,  presence: true, format: { with: /\A[ァ-ヶー]+\z/ }
+  validates :last_name_kana,   presence: true, format: { with: /\A[ァ-ヶー]+\z/ }
   validates :birth_day,        presence: true
 end

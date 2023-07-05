35cf1fb0989af3f37ed871d44d3c6fa3d92208a0
kazumakabashima <k.kazuma1028@gmail.com>
2023-07-05 19:50:17 +0900

    アクティブハッシュ　バリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,5 +11,9 @@ class Item < ApplicationRecord
   belongs_to :shipping_day 
 
   #ジャンルの選択が「---」の時は保存できないようにする
-  validates :genre_id numericality: { other_than: 1 , message: "can't be blank" } 
+  validates :category_id     numericality: { other_than: 1 , message: "can't be blank" } 
+  validates :status_id       numericality: { other_than: 1 , message: "can't be blank" } 
+  validates :burden_id       numericality: { other_than: 1 , message: "can't be blank" } 
+  validates :prefecture_id   numericality: { other_than: 1 , message: "can't be blank" } 
+  validates :shipping_day_id numericality: { other_than: 1 , message: "can't be blank" } 
 end

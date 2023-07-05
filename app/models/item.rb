e0a6b4586eea5a28c246f36c774118d282982d1e
kazumakabashima <k.kazuma1028@gmail.com>
2023-07-05 20:24:24 +0900

    新規投稿画面までの遷移

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,7 @@
 class Item < ApplicationRecord
   has_one_attached :image
   belongs_to       :user
-  has_one          :order
+  # has_one          :order
 
   validates :image, presence: true
   validates :name,  presence: true
@@ -19,9 +19,10 @@ class Item < ApplicationRecord
   belongs_to :shipping_day 
 
   #ジャンルの選択が「---」の時は保存できないようにする
-  validates :category_id     numericality: { other_than: 1 , message: "can't be blank" } 
-  validates :status_id       numericality: { other_than: 1 , message: "can't be blank" } 
-  validates :burden_id       numericality: { other_than: 1 , message: "can't be blank" } 
-  validates :prefecture_id   numericality: { other_than: 1 , message: "can't be blank" } 
-  validates :shipping_day_id numericality: { other_than: 1 , message: "can't be blank" } 
+  validates :category_id,     numericality: { other_than: 1 , message: "can't be blank" } 
+  validates :status_id,       numericality: { other_than: 1 , message: "can't be blank" } 
+  validates :burden_id,       numericality: { other_than: 1 , message: "can't be blank" } 
+  validates :prefecture_id,   numericality: { other_than: 1 , message: "can't be blank" } 
+  validates :shipping_day_id, numericality: { other_than: 1 , message: "can't be blank" } 
+
 end

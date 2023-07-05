4d6e2f6b6266c232cc794bef4d43ab0bf6019893
kazumakabashima <k.kazuma1028@gmail.com>
2023-07-05 18:28:57 +0900

    アクティブストレージの導入

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,14 @@
 FactoryBot.define do
   factory :item do
-    
+    image           { "テストイメージ" }
+    name            { Faker::Name.initials(number:2)}
+    description     { "テストテキスト"}
+    category_id     { 1 }
+    status_id       { 1 }
+    burden_id       { 1 }
+    prefecture_id   { 1 }
+    shipping_day_id { 1 }
+    price           { 500 }
+    user_id         { 1 }
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,11 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :request do
+
+  before do
+    @item = FactoryBot.build(:item)
+  end
   
   describe '商品出品機能' do
 
     context '商品出品ができる時' do
       it '正常系' do
+        expect(@item).to be_valid
       end
     end
 

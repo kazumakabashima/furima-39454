31bbe6e94eee15311e4c3edfad42e0cfd35387f6
kazumakabashima <k.kazuma1028@gmail.com>
2023-07-05 18:33:14 +0900

    items アソシエーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,12 @@
 class Item < ApplicationRecord
   has_one_attached :image
+  belongs_to       :user
+  has_one          :order
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status 
+  belongs_to :burden
+  belongs_to :prefecture
+  belongs_to :shipping_day 
 end

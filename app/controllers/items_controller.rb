5e15af02a77b1b29ad8054d53343dfa0fc01b29f
kazumakabashima <k.kazuma1028@gmail.com>
2023-07-05 21:47:12 +0900

    商品出品途中
    JSとテスト

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,7 +4,23 @@ class ItemsController < ApplicationController
     @items = Item.all
   end 
 
-  def def new
+  def new
     @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end 
+
+  private
+  def item_params
+    params.require(:item).permit(
+      :image, :name, :description, :category_id, :status_id, :burden_id, :prefecture_id, :shipping_day_id, :price
+    ).merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -3,13 +3,11 @@ class Item < ApplicationRecord
   belongs_to       :user
   # has_one          :order
 
-  validates :image, presence: true
-  validates :name,  presence: true
-  validates :price, presence: true, 
-                    numericality: {
-                      greater_than_or_equal_to: 300,
-                      less_than_or_equal_to: 9999999,
-                    }
+  validates :image,       presence: true
+  validates :name,        presence: true
+  validates :description, presence: true
+  validates :price      , presence: true, numericality: {greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
+
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
@@ -19,10 +17,10 @@ class Item < ApplicationRecord
   belongs_to :shipping_day 
 
   #ジャンルの選択が「---」の時は保存できないようにする
-  validates :category_id,     numericality: { other_than: 1 , message: "can't be blank" } 
-  validates :status_id,       numericality: { other_than: 1 , message: "can't be blank" } 
-  validates :burden_id,       numericality: { other_than: 1 , message: "can't be blank" } 
-  validates :prefecture_id,   numericality: { other_than: 1 , message: "can't be blank" } 
-  validates :shipping_day_id, numericality: { other_than: 1 , message: "can't be blank" } 
+  validates :category_id,     numericality: { other_than: 1 , message: "category can't be blank" } 
+  validates :status_id,       numericality: { other_than: 1 , message: "status can't be blank" } 
+  validates :burden_id,       numericality: { other_than: 1 , message: "burden can't be blank" } 
+  validates :prefecture_id,   numericality: { other_than: 1 , message: "prefecture can't be blank" } 
+  validates :shipping_day_id, numericality: { other_than: 1 , message: "shipping_day can't be blank" } 
 
 end

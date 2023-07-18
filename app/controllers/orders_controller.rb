2165df23b0f60f857d5d4a3ea873a7974fea5f12
kazumakabashima <k.kazuma1028@gmail.com>
2023-07-19 01:23:42 +0900

    従来の手法での保存成功

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,7 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!
   before_action :move_to_index
-  
+
   def index
     @item = Item.find(params[:item_id])
   end 
@@ -10,10 +10,20 @@ class OrdersController < ApplicationController
   end
 
   def create
-    binding.pry
+    @order = Order.create(order_params)
+    Address.create(address_params)
+    redirect_to root_path
   end
 
   private
+  def order_params
+    params.permit(:item_id).merge(user_id: current_user.id)
+  end
+
+  def address_params
+    params.permit(:postal_code, :prefecture_id, :city, :block, :phone_number).merge(order_id: @order.id)
+  end
+
   def move_to_index
     @item = Item.find(params[:item_id])
     if current_user.id == @item.user_id

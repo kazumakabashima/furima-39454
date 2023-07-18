4e2198a8d88eeae500f78bec2cee47436c090b3e
kazumakabashima <k.kazuma1028@gmail.com>
2023-07-19 03:35:12 +0900

    正常系クリア！！

--- spec/factories/order_addresses.rb
+++ spec/factories/order_addresses.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :order_address do
-    
+    postal_code { "111-1111" }
+    city {"fukuoka"}
+    block {"fukuoka"}
+    phone_number {"09011112222"}
+    prefecture_id{2}
   end
 end
--- spec/models/order_address_spec.rb
+++ spec/models/order_address_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe OrderAddress, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '購入情報の保存' do
+    before do
+      user = FactoryBot.create(:user)
+      item = FactoryBot.create(:item)
+      @donation_address = FactoryBot.build(:donation_address, user_id: user.id, item_id: item.id)
+    end
+  end
+
+    context '内容に問題ない場合' do
+      it 'すべての値が正しく入力されていれば保存できること' do
+      end
+      it 'building_nameは空でも保存できること' do
+      end
+    end
+
+
 end
+

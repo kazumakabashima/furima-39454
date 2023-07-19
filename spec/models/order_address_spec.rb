bbf82bbbb58b4a0ef83401e94767e918458bce5f
kazumakabashima <k.kazuma1028@gmail.com>
2023-07-19 16:26:23 +0900

    formObjectパターン対応開始

--- spec/models/order_address_spec.rb
+++ spec/models/order_address_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 RSpec.describe OrderAddress, type: :model do
   describe '購入情報の保存' do
     before do
-      user = FactoryBot.create(:user) # ユーザーを作成
-      item = FactoryBot.create(:item, user: user) 
+      user = FactoryBot.create(:user) 
+      item = FactoryBot.create(:item) 
       @order_address = FactoryBot.build(:order_address, user_id: user.id, item_id: item.id)
     end
     
@@ -23,7 +23,7 @@ RSpec.describe OrderAddress, type: :model do
       it 'postal_codeが空だと保存できないこと' do
         @order_address.postal_code = nil
         @order_address.valid?
-        expect(@order_address.errors.full_messages).to include("postal_code can't be blank")
+        expect(@order_address.errors.full_messages).to include("Postal_code can't be blank")
       end
     end
   end

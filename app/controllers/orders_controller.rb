36108a593d5f0f1ab4b31511e15377f79ac93acb
kazumakabashima <k.kazuma1028@gmail.com>
2023-07-19 19:32:08 +0900

    PayJPいい感じ

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -14,6 +14,7 @@ class OrdersController < ApplicationController
   def create
     @order_address = OrderAddress.new(order_params)
     if @order_address.valid?
+      pay_item
       @order_address.save
       redirect_to root_path
     else
@@ -23,7 +24,7 @@ class OrdersController < ApplicationController
 
   private
   def order_params
-    params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :block, :building_name, :phone_number).merge(item_id: params[:item_id], user_id: current_user.id)
+    params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :block, :building_name, :phone_number).merge(item_id: params[:item_id], user_id: current_user.id, token: params[:token])
   end
 
   def move_to_index
@@ -31,5 +32,15 @@ class OrdersController < ApplicationController
     if current_user.id == @item.user_id
       redirect_to root_path
     end
+  end
+
+  def pay_item
+    @item = Item.find(params[:item_id])
+    Payjp.api_key = "sk_test_e230d303f462d052f3daed89"  # 自身のPAY.JPテスト秘密鍵を記述しましょう
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: order_params[:token],    # カードトークン
+      currency: 'jpy'                 # 通貨の種類（日本円）
+    )
   end
 end
--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,7 +1,6 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture_id, :city, :block, :building_name, :phone_number, :order_id, :item_id, :user_id
-
+  attr_accessor :postal_code, :prefecture_id, :city,:block, :building_name, :phone_number, :order_id, :item_id, :user_id, :token
   with_options presence: :true do
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :city 

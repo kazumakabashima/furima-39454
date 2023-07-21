68fba7d41a4afe4324ff75e55bc463e2389bdf6c
kazumakabashima <k.kazuma1028@gmail.com>
2023-07-21 14:15:28 +0900

    購入機能修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,16 +1,12 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!
-  before_action :move_to_index
+  before_action :find_item, only: [:index, :create]
+  before_action :move_to_index, only: [:index, :create]
+  before_action :set_gon_public_key, only: [:index]
 
   def index
-    gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
-    @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new
   end 
-
-  def new
-    @order_address = OrderAddress.new
-  end
 
   def create
     @order_address = OrderAddress.new(order_params)
@@ -24,24 +20,31 @@ class OrdersController < ApplicationController
   end
 
   private
+
   def order_params
     params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :block, :building_name, :phone_number).merge(item_id: params[:item_id], user_id: current_user.id, token: params[:token])
   end
 
-  def move_to_index
+  def find_item
     @item = Item.find(params[:item_id])
+  end
+
+  def move_to_index
     if current_user.id == @item.user_id || @item.order.present?
       redirect_to root_path
     end
   end
 
   def pay_item
-    @item = Item.find(params[:item_id])
     Payjp.api_key = ENV["PAYJP_SECRET_KEY"] 
     Payjp::Charge.create(
       amount: @item.price,
       card: order_params[:token],
       currency: 'jpy'
     )
+  end
+
+  def set_gon_public_key
+    gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
   end
 end
